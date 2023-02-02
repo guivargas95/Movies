90003c550764a46fac33c80fdfac4566b9ab1965
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-02 12:58:47 -0300

    Router Dom Config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/home'
 import './index.css'
+import Page2 from './pages/page2';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="page2" element={<Page2 />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
