2e5df7996d22be7d22c6e2609a52de32d7e1e24c
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-04 19:17:33 -0300

    created type for movies json response

---
+++ src/types/MoviesType.ts
@@ -0,0 +1,10 @@
+export interface iMovies {
+    
+    id: string;
+    backdrop_path: string;
+    original_title: string;
+    overview: string;
+    title: string;
+    poster_path: string;
+    vote_avarage: number;
+}
