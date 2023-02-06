8eda84eed51d0836d0936c9501cf6e55ab4d2c73
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-06 07:57:51 -0300

    Development of responsive components

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,7 +1,7 @@
 export default function Banner(){
     return(
         <div>
-            <img className="" src="/img/banner.jpg" alt="" />
+            <img className="h-96 w-full" src="/img/banner.jpg" alt="" />
         </div>
     )
 }
--- src/components/movies/index.tsx
+++ src/components/movies/index.tsx
@@ -16,11 +16,11 @@ export default function Movies() {
                 <h2>Top 20 best rated movies!</h2>
                 <input type="SEARCH" name="" id="" />
             </div>
-            <div>
-                <ul className="flex flex-col text-center">
+            <div className="flex flex-col">
+                <ul className="flex flex-col text-center md:grid md:grid-cols-2 lg:grid-cols-3">
                     {movies?.map(response => {
                         return (
-                            <li className="flex flex-col justify-center bg-black text-white mt-5 mb-5" key={response.id}>
+                            <li className="flex flex-col justify-center bg-black text-white mt-5 mb-5 md:mr-3 md:ml-3" key={response.id}>
                                 <h3 className="text-2xl mt-3 mb-3">{response.title}</h3>
                                 <img src={`${image_path}${response.poster_path}`} alt="" />
                                 <p className="mt-3 mb-3">{response.overview}</p>
