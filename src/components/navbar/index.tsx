b937b9f39e5b3082eab9e4e72046bcd51567fc2e
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-06 13:09:26 -0300

    navbar contact link included

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,12 +1,12 @@
 export default function Navbar() {
     return (
         <div className="h-32 bg-black">
-            <div className="text-white flex justify-around font-nav font-bold text-lg">
+            <div className="text-white flex justify-around font-nav">
                 <img className="w-24 mt-4" src="\img\logo.png" alt="" />
                 <nav>
-                    <ul className="flex mt-12">
-                        <li className="ml-6 mr-6">Home</li>
-                        <li className="ml-6 mr-6">Contact</li>
+                    <ul className="flex mt-12 font-bold text-2xl">
+                        <li className="ml-6 mr-6 lg:ml-28 lg:mr-28"><a href="">Home</a></li>
+                        <li className="ml-6 mr-6 lg:ml-28 lg:mr-28"><a href="https://portfolio-react-vite.vercel.app/" target="_blank">Contact</a></li>
                     </ul>
                 </nav>
             </div>
