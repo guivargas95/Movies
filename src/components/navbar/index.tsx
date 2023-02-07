1a2038ec6a4d30dfd166a60b48aaf253a997db85
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-07 18:52:31 -0300

    changed home link

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
     return (
         <div className="h-32 bg-black">
@@ -5,7 +7,7 @@ export default function Navbar() {
                 <img className="w-24 mt-4" src="\img\logo.png" alt="" />
                 <nav>
                     <ul className="flex mt-12 font-bold text-2xl">
-                        <li className="ml-6 mr-6 lg:ml-28 lg:mr-28"><a href="">Home</a></li>
+                        <li className="ml-6 mr-6 lg:ml-28 lg:mr-28"><Link to="/">Home</Link></li>
                         <li className="ml-6 mr-6 lg:ml-28 lg:mr-28"><a href="https://portfolio-react-vite.vercel.app/" target="_blank">Contact</a></li>
                     </ul>
                 </nav>
