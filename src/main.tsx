18d97b7a66d12f834e5bb212a72dd5cf3baa9353
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-08 12:16:52 -0300

    added component to auto scroll page to top

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,13 @@ import {
 import Home from './pages/home'
 import './index.css'
 import MoviePage from './pages/moviePage';
+import ScrollToTop from './components/scrollToTop';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+    <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="moviepage" element={<MoviePage />} />
