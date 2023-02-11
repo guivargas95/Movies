a8671a937fba00be03ba8e677dd631dcbffe862e
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-11 17:19:53 -0300

    created hook to receive number of pages information

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,15 +1,18 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { iPageMovies } from "../types/PageMoviesType";
 
 export function useFetch<T = unknown>(url: string) {
 
     const [data, setData] = useState<T | null>(null)
+    const [pageData, setPageData] = useState<T | null>(null)
     useEffect(() => {
         axios.get(url)
             .then(response => {
                 setData(response.data.results);
+                setPageData(response.data);
             })
     }, [url])
 
-    return { data }
+    return { data, pageData }
 }
