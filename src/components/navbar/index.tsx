cc0eaafc409a7e3abbb41fcc4b4abd807149a00d
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-13 20:29:12 -0300

    Included hover to navbar options

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -4,11 +4,11 @@ export default function Navbar() {
     return (
         <div className="h-32 bg-black">
             <div className="text-white flex justify-around font-nav">
-                <img className="w-24 mt-4" src="\img\logo.png" alt="" />
+                <img className="w-24 mt-4" src="\img\logo.png" alt="The Movies Logo" />
                 <nav>
                     <ul className="flex mt-12 font-bold text-2xl">
-                        <li className="ml-6 mr-6 lg:ml-28 lg:mr-28"><Link to="/">Home</Link></li>
-                        <li className="ml-6 mr-6 lg:ml-28 lg:mr-28"><a href="https://portfolio-react-vite.vercel.app/" target="_blank">Contact</a></li>
+                        <li className="ml-6 mr-6 lg:ml-10 lg:mr-10 hover:text-color1"><Link to="/">Home</Link></li>
+                        <li className="ml-6 mr-6 lg:ml-10 lg:mr-10 hover:text-color1"><a href="https://portfolio-react-vite.vercel.app/" target="_blank">Contact</a></li>
                     </ul>
                 </nav>
             </div>
